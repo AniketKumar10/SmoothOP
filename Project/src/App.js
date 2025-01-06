e664674c5a2057302185557db600e7919dca5d83
AniketKumar10 <aniket19022003@gmail.com>
2025-01-06 13:25:18 +0530

    Initialize project structure with React setup, including Babel configuration, HTML templates, and basic styling

---
+++ Project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>SmoothOp</h1>
+      <button>Analyze Page</button>
+      <button>Fill Out</button>
+    </div>
+  );
+};
+
+export default App;
---
+++ Project/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "public/manifest.json", to: "manifest.json" }
+      ],
+    }),
+  ]
+};
